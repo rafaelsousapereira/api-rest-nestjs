cb619c9de71b1c8ed859fea0a4a3a4474196ae25
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-24 18:11:40 -0300

    feat: add dto and config database

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,1 +1,13 @@
-export class CreateAccountDto {}
+import { UUID } from "crypto";
+
+enum AccountType  {
+  Current = "CURRENT",
+  Saving = "SAVING",
+}
+
+export class CreateAccountDto {
+  id: UUID;
+  name: string;
+  accountType: AccountType;
+  balance: number;
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AccountsModule } from './accounts/accounts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AccountsModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: 'admin',
+    database: 'api_nestjs',
+    entities: [],
+    synchronize: true,
+    logging: true,
+  }),
+  AccountsModule],
   controllers: [],
   providers: [],
 })
