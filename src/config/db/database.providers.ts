7636a892084cd10699a1e84d6af3aae2281f15ae
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-06-26 21:35:21 -0300

    feat: add config database

---
+++ src/config/db/database.providers.ts
@@ -0,0 +1,23 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE,
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
